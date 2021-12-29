5eb17742f61a8bd105e9c8b29f73c20999c7d7f4
Isaac Barr <isaacjbarr@hotmail.co.uk>
2021-12-29 22:01:18 +0000

    Start of landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { StudentInputComponent } from './landing-page/student-input/student-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LandingPageComponent,
+    StudentInputComponent
   ],
   imports: [
     BrowserModule,
